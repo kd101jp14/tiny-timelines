7763484736070de3f70593eb5850118152c38f62
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-06-20 12:26:33 -0500

    Set up redux boilerplate code in 'store.js'.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-
+import { Provider } from "react-redux";
+import store from "./store";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
@@ -10,6 +11,7 @@ import Login from "./components/auth/Login";
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Router>
         <div className="App">
           <Navbar />
@@ -18,6 +20,7 @@ class App extends Component {
           <Route exact path="/login" component={Login} />
         </div>
       </Router>
+      </Provider>
     );
   }
 }
