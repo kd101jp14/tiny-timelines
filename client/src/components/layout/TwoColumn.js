3ec6a67ccefac927747050453ed8dcb350561173
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-06-25 14:44:10 -0500

    Created navigation buttons for the 'TwoColumn' layout.

--- client/src/components/layout/TwoColumn.js
+++ client/src/components/layout/TwoColumn.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./TwoColumn.css";
 
 class TwoColumnLayout extends Component {
@@ -6,7 +7,58 @@ class TwoColumnLayout extends Component {
     return (
       <div className="two-column-container">
         <div className="left-column">
-          sdsds
+        <div className="col s6">
+            <Link
+              to="/dashboard"
+              style={{
+                width: "140px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large waves-effect waves-light hoverable teal lighten-2"
+            >
+              Home
+            </Link>
+          </div>
+          <div className="col s6">
+            <Link
+              to="/stories"
+              style={{
+                width: "140px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large waves-effect waves-light hoverable teal lighten-2"
+            >
+              Stories
+            </Link>
+          </div>
+          <div className="col s6">
+            <Link
+              to="/photos"
+              style={{
+                width: "140px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large waves-effect waves-light hoverable teal lighten-2"
+            >
+              Photos
+            </Link>
+          </div>
+          <div className="col s6">
+            <Link
+              to="/trackers"
+              style={{
+                width: "140px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px"
+              }}
+              className="btn btn-large waves-effect waves-light hoverable teal lighten-2"
+            >
+              Trackers
+            </Link>
+          </div>
         </div>
         <div className="right-column">{this.props.children}</div>
       </div>
