6c5ef819c214193dc0ca80a012909da5f36a7456
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-06-27 18:40:06 -0500

    The user's input in the story form is now cleared after submission.

--- client/src/components/forms/StoryForm.js
+++ client/src/components/forms/StoryForm.js
@@ -3,47 +3,51 @@ import PropTypes from "prop-types";
 import "./StoryForm.css";
 
 class StoryForm extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    this.state = {
+      inputVal: ""
+    };
+  }
 
-        this.state= {
-            inputVal: ""
-        }
-    }
+  valueChange = event => {
+    this.setState({ inputVal: event.target.value });
+  };
 
-    valueChange = (event) => {
-        this.setState({inputVal: event.target.value});
-    }
-
-    submitForm = (event) => {
-        console.log("Clicked!", this.state.inputVal);
-        this.props.onSubmit(this.state.inputVal);
-    }
+  submitForm = () => {
+    console.log("Clicked!", this.state.inputVal);
+    this.props.onSubmit(this.state.inputVal);
+    this.setState({ inputVal: "" });
+  };
 
   render() {
     return (
       <div className="story-form-container">
-          <div>
-            <input 
-                type="text" 
-                placeholder="Today..." 
-                onChange={this.valueChange} 
-                value={this.state.inputVal}
-            />
-          </div>
-          <div>
-            <button className="btn" onClick={this.submitForm}>
-                Submit
-            </button>
-          </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Today..."
+            onChange={this.valueChange}
+            value={this.state.inputVal}
+            style={{
+              color: "rgba(1, 55, 105, 1)",
+              fontSize: "20px"
+            }}
+          />
+        </div>
+        <div>
+          <button className="btn" onClick={this.submitForm}>
+            Submit
+          </button>
+        </div>
       </div>
     );
   }
 }
 
 StoryForm.propTypes = {
-    onSubmit: PropTypes.func.isRequired
+  onSubmit: PropTypes.func.isRequired
 };
 
 export default StoryForm;
