cb8c7699831cebc9aba7c462f11decd727a6968d
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-07-10 21:35:32 -0500

    add build files!

---
+++ client/build/precache-manifest.abbe7d29a7f6364ede06b682c363492c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d789584d71655b4c3c92031f01c96e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf1cc0e7b07fb8addf9",
+    "url": "/static/css/2.c465c46e.chunk.css"
+  },
+  {
+    "revision": "7a87f2063b739bb737b9",
+    "url": "/static/css/main.c7a0cad1.chunk.css"
+  },
+  {
+    "revision": "2cf1cc0e7b07fb8addf9",
+    "url": "/static/js/2.92956777.chunk.js"
+  },
+  {
+    "revision": "7a87f2063b739bb737b9",
+    "url": "/static/js/main.0edf515c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
